4d8480a4218987ce61a83d835c1f0f15cb25ce50
ziad Elshuwaikh <ziadelshuwaikh@gmail.com>
2025-07-19 21:36:06 +0300

    Remove temporary files (bin, obj, .vs) and clean up project

--- Netflix.API/Netflix.API/DTOs/UserDtos/RegisterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Netflix.API.DTOs.UserDtos
-{
-    public class RegisterDTO
-    {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
