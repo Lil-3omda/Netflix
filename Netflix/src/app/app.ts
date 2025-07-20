83b90a8e48c12adbe2c951ad768c3d7c4780475f
abdallabelal <belalabdalla29@gmail.com>
2025-07-20 17:58:21 +0300

    Design Home page

--- Netflix/src/app/app.ts
+++ Netflix/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { Home } from './features/videos/home/home';
+import { MovieSliderSectionComponent } from './shared/movie-slider/movie-slider';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar,Home,MovieSliderSectionComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
