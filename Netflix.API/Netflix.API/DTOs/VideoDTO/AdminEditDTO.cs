72d2214ae791c26b1f6062b9019007f7682cfd6b
ibrahimmoustafa99 <elhadidyibra@gmail.com>
2025-07-29 10:22:34 +0300

    restyle category page and maintenance updating & softDelete and restore of movies

---
+++ Netflix.API/Netflix.API/DTOs/VideoDTO/AdminEditDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Netflix.API.DTOs.VideoDTO
+{
+    public class AdminEditDTO
+    {
+        public string Description { get; set; }
+
+        public string TrailerUrl { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
